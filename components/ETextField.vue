5d182f25664d0437be960e5210ab7de1a2e59c54
leju-Steven <steven@leju.com.tw>
2025-11-22 00:56:58 +0800

    feat: 修改互動及dialog 確認框

--- components/ETextField.vue
+++ components/ETextField.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="flex flex-col space-y-1 w-full">
-    <label :for="props.id">{{ label }}</label>
-    <input 
-      :id="props.id" 
+    <label v-if="!isEmpty(label)" :for="props.id">{{ label }}</label>
+    <input
+      :id="props.id"
       :type="props.inputType"
       :value="props.inputValue"
-      class="border-1px text-white border-gray-200 text-16px  bg-transparent rounded px-2 py-2 outline-none appearance-none m-0 shadow-none" 
+      class="border-1px text-white border-gray-200 text-16px bg-transparent rounded px-2 py-2 outline-none appearance-none m-0 shadow-none"
+      :class="isTextCenter ? 'text-center' : ''"
       @input="$emit('update:input-value', $event.target.value)"
     />
   </div>
@@ -19,12 +20,18 @@ interface Props {
   label?: string
   inputType?: string
   inputValue: string | number
+  isTextCenter?: boolean
 }
 
 const props = withDefaults(defineProps<Props>(), {
   inputType: 'text',
-  id: () => uuidv4()
+  isTextCenter: false,
+  id: () => uuidv4(),
 })
+
+const isEmpty = (value: string | undefined): boolean => {
+  return !value || value.trim().length === 0
+}
 </script>
 
 <style scoped lang="scss">
--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
       <section
         class="max-w-640px min-h-360px flex flex-col items-center text-white mx-auto border border-coolGray rounded-2xl border-solid px-20"
       >
-        <h2 class="text-2xl font-semibold mb-4">操作</h2>
+        <div class="text-2xl font-semibold mb-4">操作</div>
         <div class="mb-6 w-full">
           <ETextField v-model:input-value="inputFormData.name" id="user-name" label="名字" />
         </div>
@@ -36,7 +36,7 @@
 
       <!-- 用戶資料清單 -->
       <section
-        class="max-w-640px min-h-360px flex py-4 flex-col items-center text-white mx-auto border border-coolGray rounded-2xl border-solid px-20"
+        class="max-w-640px min-h-360px flex py-4 flex-col items-center text-white mx-auto border border-coolGray rounded-2xl border-solid px-10"
       >
         <table>
           <thead class="text-18px">
@@ -51,12 +51,31 @@
           <tbody>
             <template v-for="(user, index) in userList" :key="user?.id || `user-${index}`">
               <tr class="text-center">
-                <td class="px-8 py-2">{{ user.id }}</td>
-                <td class="px-8 py-2">{{ user.name }}</td>
-                <td class="px-8 py-2">{{ user.age }}</td>
-                <td class="px-8 py-2 flex space-x-2">
-                  <EBtn text="修改" @click="modifyUser(12, 'test', 41)"  />
-                  <EBtn text="刪除" color="error" @click="deleteUser(user.id)" />
+                <td class="px-8 py-2 align-middle">{{ user.id }}</td>
+                <td class="px-8 py-2 align-middle">
+                  <div v-if="currentEditUserId === user.id" class="w-24">
+                    <ETextField v-model:input-value="editedFormData.name" :is-text-center="true" />
+                  </div>
+                  <div class="w-24" v-else>{{ user.name }}</div>
+                </td>
+                <td class="px-8 py-2 align-middle">
+                  <div v-if="currentEditUserId === user.id" class="w-10">
+                    <ETextField v-model:input-value="editedFormData.age" :is-text-center="true" />
+                  </div>
+                  <div class="w-10" v-else>{{ user.age }}</div>
+                </td>
+                <td class="px-8 py-2 align-middle">
+                  <div class="flex space-x-2 justify-center">
+                    <template v-if="currentEditUserId === user.id">
+                      <EBtn text="儲存" @click="openDialog" />
+                      <EBtn text="取消" color="warn" @click="cancelEdit" />
+                    </template>
+
+                    <template v-else>
+                      <EBtn text="修改" @click="handleModifyUser(user.id)" />
+                      <EBtn text="刪除" color="error" @click="deleteUser(user.id)" />
+                    </template>
+                  </div>
                 </td>
               </tr>
             </template>
@@ -64,6 +83,23 @@
         </table>
       </section>
     </main>
+
+    <div>{{ isDialogOpen }}</div>
+
+    <dialog ref="dialogRef" class="rounded-lg space-y-4 text-center p-8 bg-gray-800 text-white">
+      <div class="text-18px">確認是否進行以下修改?</div>
+
+      <div>
+        <div>#{{ editedFormData.id }}</div>
+        <div>名字：{{ editedFormData.name }}</div>
+        <div>年齡：{{ editedFormData.age }}</div>
+      </div>
+
+      <div class="flex space-x-2">
+        <EBtn text="取消" color="warn" @click="cancelEdit" />
+        <EBtn text="確認修改" />
+      </div>
+    </dialog>
   </div>
 </template>
 
@@ -77,15 +113,76 @@ const { userList } = storeToRefs(appStore)
 
 const baseUrl = 'https://2869.wu.elitepro.ltd' // 後端網址 將由面試官提供
 
+// Dialog 控制
+const dialogRef = ref<HTMLDialogElement | null>(null)
+const isDialogOpen = ref(false)
+
+const openDialog = () => {
+  dialogRef.value?.showModal()
+  isDialogOpen.value = true
+}
+
+const closeDialog = () => {
+  dialogRef.value?.close()
+  isDialogOpen.value = false
+}
+
+watchEffect(() => {
+  dialogRef.value?.addEventListener('close', () => {
+    isDialogOpen.value = false
+  })
+})
+
 const inputFormData = ref({
   name: '',
-  age: '',
+  age: '' as number | string,
 })
 
 const errorMessage = ref({
   name: '',
   age: '',
 })
+
+// === 編輯使用者相關 ===
+const currentEditUserId = ref<number | null>(null)
+const editedFormData = ref({
+  id: 0,
+  name: '',
+  age: '' as number | string,
+})
+
+const setCurrentEditUserId = (userId: number | null) => {
+  currentEditUserId.value = userId
+}
+
+const setEditedFormData = (data: { id: number; name: string; age: number | string }) => {
+  editedFormData.value = { id: data.id, name: data.name, age: data.age }
+}
+
+const useCloner = <T,>(data: T): T => {
+  return JSON.parse(JSON.stringify(data))
+}
+
+const handleModifyUser = (userId: number) => {
+  const cloneData = useCloner(userList.value.find((user) => user.id === userId))
+  if (cloneData) {
+    setCurrentEditUserId(userId)
+    setEditedFormData({
+      id: userId,
+      name: cloneData.name,
+      age: cloneData.age,
+    })
+  } else {
+    console.error('User not found for modification')
+    return
+  }
+}
+
+const cancelEdit = () => {
+  setCurrentEditUserId(null)
+  closeDialog()
+}
+// === 編輯使用者相關 end ===
 
 const validateInputName = (): boolean => {
   return inputFormData.value.name.trim().length > 0
